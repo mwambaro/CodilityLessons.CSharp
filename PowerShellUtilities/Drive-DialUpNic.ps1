ff1a03ae764ecb1da798876ced13ea578ddc6a25
Obed-Edom Nkezabahizi <onkezabahizi@gmail.com>
2021-09-08 15:56:49 +0200

    Env variable ditched in favor of serialized array. SaveHelp script stubbed with modules list serialization.

--- PowerShellUtilities/Drive-DialUpNic.ps1
+++ PowerShellUtilities/Drive-DialUpNic.ps1
@@ -11,46 +11,116 @@
 ##
 ###############################################################################################
 
+param(
+    [Parameter(ValueFromPipeline)]
+    [string]$FunctionName="",
+    [Parameter(ValueFromPipeline)]
+    [System.Collections.Hashtable]$ParameterList=@{}
+)
+
 $ModemDeviceClass = "Modem"
-$ModemServiceCommands = New-Object System.Collections.ArrayList
+$IntMax = [Math]::Pow(2, 32) - 1
 $CommandsCollectionJob = $null
+$CommandsJsonFile = "ModemCommandsJsonFile.json"
 $CommandsExecJob = $null
+$Sapi = New-Object -Com Sapi.SpVoice
+$TimerEvent = $null
 $ModemService = Get-Service | Where-Object {$_.Name -Match $ModemDeviceClass}
 
 function Make-LogFileName
 {
+    Make-FilePathName
+}
+
+function Make-FilePathName
+{
+    param(
+        [Parameter(ValueFromPipeline)]
+        [System.String]$FileName = ""
+    )
+
     $ScriptName = & {$myInvocation.ScriptName}
     $segs = $ScriptName.Split(@('/','\'))
     $scriptFileName = $segs[$segs.Count-1]
     $sName = $scriptFileName.Split('.')
     $sName = $sName[0]
-    $MyScriptLogFile = Join-Path $home "$($sName).log"
 
-    $MyScriptLogFile
+    if([string]::IsNullOrEmpty($FileName))
+    {
+        $FileName = "$($sName).log"
+    }
+
+    $MyFile = Join-Path $home $FileName
+
+    $MyFile
+}
+
+## <brief> Subscribes to timer event raised by [Timers.Timer] .NET object </brief>
+## <param name="TimerEventAction"> Action code executed when timer elapses. Defaults to no action. </param>
+## <return> The timer object created. </return>
+## <example> Subscribe-ToTimerEvent -TimerEventAction {$Sapi.Speak("Timer has elapsed")} </example>
+function Subscribe-ToTimerEvent
+{
+    param([System.Management.Automation.ScriptBlock]$TimerEventAction = {})
+
+    $srcId = "Timer.Elpased"
+    $Event = Get-EventSubscriber | Where-Object{$_.SourceIdentifier -Match "^$($srcId)$"}
+    if($Event)
+    {
+        Unregister-Event -SourceIdentifier $srcId
+    }
+    
+    $Timer = New-Object Timers.Timer
+    $Timer.Enabled = $false
+    $Timer.Autoreset = $false
+    $Timer.Interval = 10000
+
+    $objectEventArgs = @{
+        InputObject = $Timer
+        EventName = 'Elapsed'
+        SourceIdentifier = 'Timer.Elapsed'
+    }
+    Register-ObjectEvent @objectEventArgs -Action $TimerEventAction
+  
+    $Timer
 }
 
 function Collect-ModemServiceCommands
 {
-    param([bool]$AsJob = $true)
+    param(
+        [Parameter(ValueFromPipeline)]
+        [System.Collections.Hashtable]$Parameters = @{}
+    )
+    $AsJob = $false
+    $AsJob = $Parameters['AsJob']
+    $ModemServiceCommands = New-Object System.Collections.ArrayList
+    $ModemServiceCommandsFile = $CommandsJsonFile | Make-FilePathName
+
+    Write-Host "Commands to be written to [$ModemServiceCommandsFile]."
     
     $name = "Modem service commands collection"
     $job = Get-Job | Where-Object {$_.Name -Match $name}
-    if($job.State -Match "Running")
+    if($job)
     {
-        Write-Host -NoNewline -ForegroundColor Cyan "JOB [$($job.Name)] is already running. Restarting ... "
-        Stop-Job $job.Id
-        Remove-Job $job.Id
+        if($job.State -Match "Running")
+        {
+            Write-Host -NoNewline -ForegroundColor Cyan "JOB [$($job.Name)] is already running. Restarting ... "
+            Stop-Job $job.Id
+            Remove-Job $job.Id
+        }
     }
 
-    $CommandsCollectionJob = Start-Job -Name $name -ScriptBlock {
+    $jobBlock = {
         $MyLogFile = Make-LogFileName
         $cmd = 0
-
-        while($true)
+        
+        $jbBreak = $true
+        while($jbBreak)
         {
             if($ModemService)
             {
-                while($true)
+                $svcBreak = $true
+                while($svcBreak)
                 {
                     $logLine = ""
                     try
@@ -67,27 +137,42 @@ function Collect-ModemServiceCommands
                         $logLine += "[OK]"
                         Write-Host -NoNewline "["
                         Write-Host -NoNewline -ForegroundColor Green "OK"
-                        Write-Host -NoNewline "]"
+                        Write-Host -NoNewline "] [Int Max: $IntMax]"
                         Write-Host ""
                         
-                        $ModemServiceCommands.Add($cmd)
-                        if($ModemServiceCommands.Count -EQ 1)
-                        {
-                            Exec-ModemServiceCommands
-                        }
+                        ModemServiceCommands.Add($cmd)
+                        $ModemServiceCommandsJson = ModemServiceCommands | ConvertTo-Json -Compress
+                        [System.IO.File]::WriteAllText($ModemServiceCommandsFile, $ModemServiceCommandsJson)
                     }
                     catch
                     {
                         $logLine += "[ERROR]"
                         Write-Host -NoNewline "["
                         Write-Host -NoNewline -ForegroundColor Red "ERROR"
-                        Write-Host -NoNewline "]"
+                        Write-Host -NoNewline "] [Int Max: $IntMax]"
                         Write-Host ""
                     }
                     
-                    [System.IO.File]::AppendAllText($MyLogFile, $logLine)
+                    #[System.IO.File]::AppendAllText($MyLogFile, $logLine)
 
-                    $cmd += 1
+                    if($cmd -GT 0 -AND $cmd -EQ $IntMax)
+                    {
+                        $cmd = -1
+                        continue
+                    }
+                    elseif ($cmd -LT 0 -AND $cmd -EQ (-$IntMax))
+                    {
+                        $svcBreak = $false
+                        $jbBreak = $false
+                    }
+                    
+                    if($svcBreak)
+                    { 
+                        if($cmd -GE 0) 
+                        { $cmd += 1 }
+                        else 
+                        { $cmd -= 1}
+                    }
                     #sleep 1
                 }
             }
@@ -97,31 +182,65 @@ function Collect-ModemServiceCommands
                 sleep 1
             }
         }
+
+        $verbose = "Job '$($name)' is finished."
+        $TimerEvent = Subscribe-ToTimerEvent -TimerEventAction {
+            $Sapi.Speak($verbose)
+        }
+        if($TimerEvent)
+        {
+            $TimerEvent.Enabled = $true
+            $TimerEvent.Interval = 10000
+            $TimerEvent.Autoreset = $true
+        }
+    }
+    
+    $CommandsCollectionJob = $null
+    if($AsJob)
+    {
+        Write-Host -NoNewline "Executing code as a background job ... "
+        $CommandsCollectionJob = Start-Job -Name $name -ScriptBlock $jobBlock
+    }
+    else
+    {
+        Write-Host -NoNewline "Executing code in current console ... "
+        . $jobBlock
     }
 
     Write-Host -NoNewline -ForegroundColor Green "[OK]"
-    Write-Host " [ID: $($CommandsCollectionJob.Id)]"
+    if($AsJob) { Write-Host " [ID: $($CommandsCollectionJob.Id)]" }
 }
 
 function Exec-ModemServiceCommands
 {
-    param([bool]$AsJob = $true)
+    param(
+        [Parameter(ValueFromPipeline)]
+        [System.Collections.Hashtable]$Parameters = @{}
+    )
+    $AsJob = $false
+    $AsJob = $Parameters['AsJob']
     $name = "Modem service commands execution"
+    $ModemServiceCommandsFile = $CommandsJsonFile | Make-FilePathName
 
     $job = Get-Job | Where-Object {$_.Name -Match $name}
-    if($job.State -Match "Running")
+    if($job)
     {
-        Write-Host -NoNewline -ForegroundColor Cyan "JOB [$($job.Name)] is already running. Restarting ... "
-        Stop-Job $job.Id
-        Remove-Job $job.Id
+        if($job.State -Match "Running")
+        {
+            Write-Host -NoNewline -ForegroundColor Cyan "JOB [$($job.Name)] is already running. Restarting ... "
+            Stop-Job $job.Id
+            Remove-Job $job.Id
+        }
     }
     
-    $CommandsExecJob = Start-Job -Name $name -ScriptBlock {
+    $jobBlock = {
         $MyLogFile = Make-LogFileName
         $oldLength = 0
+        $ModemServiceCommandsJson = [System.IO.File]::ReadAllText($ModemServiceCommandsFile)
+        $ModemServiceCommands = $ModemServiceCommandsJson | ConvertFrom-Json
     
-        while(1)
-        {
+        while($true)
+        {         
             if($ModemServiceCommands -AND $ModemService)
             {
                 $length = $ModemServiceCommands.Count
@@ -152,7 +271,7 @@ function Exec-ModemServiceCommands
                         Write-Host -NoNewline "]"
                         Write-Host ""
 
-                        [System.IO.File]::AppendAllText($MyLogFile, $logLine)
+                        #[System.IO.File]::AppendAllText($MyLogFile, $logLine)
                         
                         $idx += 1
                         sleep 1
@@ -164,13 +283,30 @@ function Exec-ModemServiceCommands
             sleep 1
         }
     }
+    
+    $CommandsExecJob = $null
+    if($AsJob)
+    {
+        Write-Host -NoNewline "Executing code as a background job ... "
+        $CommandsExecJob = Start-Job -Name $name -ScriptBlock $jobBlock
+    }
+    else
+    {
+        Write-Host -NoNewline "Executing code in current console ... "
+        . $jobBlock
+    }
 
     Write-Host -NoNewline -ForegroundColor Green "[OK]"
-    Write-Host " [ID: $($CommandsExecJob.Id)]"
+    if($AsJob) { Write-Host " [ID: $($CommandsExecJob.Id)]" }
 }
 
 function Get-DialUpNicDriverDetails
 {
+    param(
+        [Parameter(ValueFromPipeline)]
+        [System.Collections.Hashtable]$Parameters = @{}
+    )
+
     # Modem devices
     $dev = Get-WmiObject Win32_PnPSignedDriver | Where-Object {$_.DeviceClass -Match $ModemDeviceClass}
     
@@ -218,10 +354,19 @@ function Main
     {
         [System.IO.File]::Delete($logfile)
     }
-
-    Get-DialUpNicDriverDetails
-    Collect-ModemServiceCommands
-    Exec-ModemServiceCommands
+    
+    if($FunctionName -Match "^Get-DialUpNicDriverDetails$")
+    {
+        $ParameterList | Get-DialUpNicDriverDetails
+    }
+    elseif($FunctionName -Match "^Collect-ModemServiceCommands$")
+    {
+        $ParameterList | Collect-ModemServiceCommands 
+    }
+    elseif($FunctionName -Match "^Exec-ModemServiceCommands$")
+    {
+        $ParameterList | Exec-ModemServiceCommands
+    }
 }
 
 Main
--- PowerShellUtilities/Hone-SaveHelpCapabilities.ps1
+++ PowerShellUtilities/Hone-SaveHelpCapabilities.ps1
@@ -1,7 +1,43 @@
-﻿################################################################################
+﻿##################################################################################
 ##  Hone-SaveHelpCapabilities.ps1
 ##      1. Write a task that lists all modules that need help info and serialize 
 ##         the collection to a file so it can natively be loaded into PS session.
 ##      2. 
 ##
-################################################################################
+##################################################################################
+
+param(
+    [Parameter(ValueFromPipeline)]
+    [string]$ModulesJsonFile=""
+)
+
+if([string]::IsNullOrEmpty($ModulesJsonFile))
+{
+    $ModulesJsonFile = Join-Path $Home "PSModulesJsonFile.json"
+}
+
+function Serialize-ModulesToFile
+{
+    Write-Host "Serializing PS module names to "
+    Write-Host -NoNewline "[$ModulesJsonFile] ... "
+
+    $Modules = New-Object System.Collections.ArrayList
+    Get-Module -All -ListAvailable | ForEach-Object {
+        if(-NOT $Modules.Contains($_.Name))
+        {
+            $Modules.Add($_.Name)
+        }
+    }
+    $ModulesJson = $Modules | Sort-Object | ConvertTo-Json -Compress
+
+    [System.IO.File]::WriteAllText($ModulesJsonFile, $ModulesJson)
+
+    Write-Host -NoNewline -ForegroundColor Green "OK"
+    Write-Host -NoNewline " ["
+    Write-Host -NoNewline -ForegroundColor Magenta "$($Modules.Count) modules"
+    Write-Host -NoNewline "]"
+
+    $ModulesJsonFile
+}
+
+$fileName = Serialize-ModulesToFile
